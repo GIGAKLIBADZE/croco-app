aa70a69307af541f37aa929f55ece5211173ea27
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-11-12 01:41:51 +0400

    add the sidebar, footer, header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from "./shared/sidebar/sidebar";
+import { HeaderComponent } from "./shared/header/header";
+import { Footer } from "./shared/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sidebar, Footer, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
